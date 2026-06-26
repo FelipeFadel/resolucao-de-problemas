6042950e2a847a7658fd01bdac0a208211190d60
Felipe Fadel <67320543+FelipeFadel@users.noreply.github.com>
2026-06-26 18:40:31 -0300

    feat: maratona

--- maratona/3499.ts
+++ maratona/3499.ts
@@ -58,3 +58,89 @@
 //     if (!isFound) console.log(name + " eh facil");
 //   }
 // });
+
+// process.stdin.resume();
+// process.stdin.setEncoding("utf8");
+
+// let input = "";
+// let lineCount = 0;
+
+// process.stdin.on("data", (chunk) => {
+//   input += chunk;
+// });
+
+// function getIndex(a: string): number {
+//   return a.charCodeAt(0) - 97;
+// }
+
+// function getDistancia(a: string, b: string): number {
+//   if (a === b) return 0;
+
+//   const indexA = getIndex(a);
+//   const indexB = getIndex(b);
+
+//   const diferenca = Math.abs(indexA - indexB);
+
+//   return Math.min(diferenca, 26 - diferenca);
+// }
+
+// process.stdin.on("end", () => {
+//   const inputI = input.trim().split("\n");
+
+//   const S = inputI[0].trim();
+//   const T = inputI[1].trim();
+
+//   let distancia = 0;
+
+//   for (let i = 0; i < S.length; i++) {
+//     distancia += getDistancia(S[i], T[i]);
+//   }
+
+//   console.log(distancia);
+// });
+
+//97
+//toLowerCase()
+
+//azur
+//aala
+
+process.stdin.resume();
+process.stdin.setEncoding("utf8");
+
+let input = "";
+let lineCount = 0;
+
+process.stdin.on("data", (chunk) => {
+  input += chunk;
+});
+
+process.stdin.on("end", () => {
+  const inputI: string[] = input.trim().split("\n");
+
+  const first = inputI[0].split(" ");
+  const N: number = Number(first[0]);
+  const L: number = Number(first[1].replace(",", "."));
+  const Q: number = Number(first[2].replace(",", "."));
+
+  const Lint = Math.round(L * 10);
+  const Qint = Math.round(Q * 10);
+
+  const names = inputI[1].split(" ");
+
+  const cabacas: number = Math.floor(Lint / Qint);
+  const resto = Lint % Qint;
+
+  let rico: number;
+  let sobra: number;
+
+  if (resto === 0) {
+    rico = (cabacas - 1) % N;
+    sobra = Q;
+  } else {
+    rico = cabacas % N;
+    sobra = resto / 10;
+  }
+
+  console.log(names[rico] + " " + sobra.toFixed(1));
+});
